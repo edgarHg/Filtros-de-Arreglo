07c5a5efdc6614a6e29165d61863ec30d6a402a4
EDGAR HERNANDEZ GARCIA <iscehg@gmail.com>
2016-09-06 09:34:12 -0500

    Integrando estilo a el tag pre

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 angular.module('App', ['serviceApp'])
 	.controller('ctrlFiltrosArray', ['$scope', 'filtrosArray',function($scope,filtrosArray){
 		var arrVerduras = [
-			{nombre:'Zanahoria', cantidad: "1Kg"},
-			{nombre:'Espinacas', cantidad: "2Kg"},
-			{nombre:'Lechuga', cantidad: "50gr"},
-			{nombre:'Cebolla', cantidad: "700gr"},
-			{nombre:'Espinacas', cantidad: "1.5Kg"},
-			{nombre:'Cebolla', cantidad: "120gr"}
-		];
+			{
+				nombre: 'Zanahoria',
+				cantidad: "1Kg"
+			}, {
+				nombre: 'Espinacas',
+				cantidad: "2Kg"
+			}, {
+				nombre: 'Lechuga',
+				cantidad: "50gr"
+			}, {   
+				nombre: 'Cebolla',
+				cantidad: "700gr"
+			}, {
+				nombre: 'Espinacas',
+				cantidad: "1.5Kg"
+			}, {
+				nombre: 'Cebolla',
+				cantidad: "120gr"
+			}];
 
-		$scope.arregloVerduras = arrVerduras;
-		$scope.arregloFiltrado = filtrosArray.unicos(arrVerduras, 'nombre');
+		$scope.arregloVerduras = angular.toJson(arrVerduras, true);
+		$scope.arregloFiltrado = angular.toJson(filtrosArray.unicos(arrVerduras, 'nombre'), true);
 	}])
